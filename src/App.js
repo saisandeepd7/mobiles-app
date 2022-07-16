1550a5b30128953c85d1c0bcfadd5ce9640fbf33
saisandeepd7 <dsandeep3690@gmail.com>
2022-07-16 15:46:31 +0530

    mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+//const API="http://localhost:4000"
+const API="https://sandy-node.herokuapp.com"
+
 function App() {
+
+  const[mobiles,setMobiles]= useState([])
+
+  useEffect(()=>{
+  fetch(`${API}/mobiles`)
+  .then((data)=>data.json())
+  .then((mbs)=>setMobiles(mbs))
+},[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='phone-list-container'>
+      {mobiles.map((mobile)=>(
+         <Phone key={mobile._id} mobile={mobile}/>
+      ))}
+      </div>
+      </div>
   );
 }
+
+
+function Phone({mobile}){
+  
+  return(
+    <div className='phone-container'>
+      <img className='phone-picture' src={mobile.img} alt={Image}></img>
+      <h2 className='phone-name'>{mobile.model}</h2> 
+      <p className='phone-company'>{mobile.company}</p>
+    </div>
+  )
+}
+
 
 export default App;
